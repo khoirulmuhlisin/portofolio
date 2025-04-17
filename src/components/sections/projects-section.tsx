0175cec636aa3a59e7a1e2a39fb154925db264ef
khoirulmuhlisin <khoerulmuhlisin3@gmail.com>
2025-04-17 10:44:34 +0700

    Update projects-section.tsx

--- src/components/sections/projects-section.tsx
+++ src/components/sections/projects-section.tsx
@@ -147,7 +147,7 @@ export function ProjectsSection() {
       description: "Our Love Story Continues – Be Part of Our Special Day.",
       image: "https://images.unsplash.com/photo-1562616261-7bb659f860ed?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       tags: ["Vite", "Tailwind CSS", "React", "Shadcn-ui", "TypeScript"],
-      link: "https://marimenikah2.vercel.app",
+      link: "https://marimenikah3.vercel.app",
       github: "#",
       year: "2025",
       featured: true
